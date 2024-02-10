79289354fee09c9e49710b59487171ff9b034db9
Rommel John Jabla <romjab2@gmail.com>
2024-02-11 07:26:24 +0800

    update harpoon

--- lua/my_custom.lua
+++ lua/my_custom.lua
@@ -1,7 +1,12 @@
 local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup()
+harpoon:setup({
+  settings = {
+       save_on_toggle = true
+  }
+})
+
 -- REQUIRED
 
 vim.keymap.set("n", "<C-a>", function() harpoon:list():append() end)
@@ -12,9 +17,9 @@ vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+-- -- Toggle previous & next buffers stored within Harpoon list
+-- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+-- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
 -- Explorer
 vim.api.nvim_set_keymap('n', '<C-o>', ':Explore<CR>', { noremap = true, silent = true })
