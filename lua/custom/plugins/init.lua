0824286f239809572f8ec33296768124aeab5274
DESKTOP-05REH6Q\Rommel <rommel.jabla@flatplanet.com.au>
2024-03-03 10:19:51 +0800

    nvim tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,10 +2,24 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
 return {
 	{
-	    "ThePrimeagen/harpoon",
-	    branch = "harpoon2",
-	    dependencies = { "nvim-lua/plenary.nvim" }
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" }
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = function ()
+			require('neo-tree').setup {}
+		end,
 	}
 }
